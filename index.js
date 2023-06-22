d910e03d6e86eaf250018c5ccc2ec69bde8268a0
xhola <ohksamie@gmail.com>
2023-06-22 09:17:30 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-var express = require('express');
-const hello = require('./api/hello');
-const resource = require('./api/resource');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
 
-var app = express();
 
-app.use(express.json());
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// settting view up engines
+// using ejs template
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use("/", resource);
-app.use("/hello", hello);
+//setting up middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.raw());
+app.use(express.static("./public"));
+app.use(express.static("./uploads"));
+// app.use(require("cors")());
 
-app.listen(3000, ()=>{
-    console.log("Server is runningAA!");
+require("./router")(app);
+
+// 404 Error Handler
+app.all("*", (req, res) => {
+    res.status(404).json({
+        status: false,
+        error: "And Just Like That, You Completely Lost Your Way 😥",
+    });
 });
+
+
+app.listen(PORT, () => {
+    console.log(
+        `Server is running on port ${PORT}
+
+    http://localhost:${PORT}`
+
+  );
+});
+
+
+
+
+
+module.exports = { app };
+
+require('./router')(app);
